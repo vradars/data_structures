8ba72ebd6472ebcc56af166f7168fa9a0a7cd011
SomyaGupta88 <guptasaumya668@gmail.com>
2019-07-12 22:57:47 +0530

    hi

---
+++ linearsearch.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(void)
+{
+	int n,i,m;
+	scanf("%d",n);
+	int arr[n];
+	int k;
+	Scanf("%d",k)
+		//ele to search
+	for(i=0;i<n;i++)
+	{
+		Scanf("%d",arr[i]);
+		if(arr[i]==k)
+		m=i;
+	}
+	printf("%d",m);
+}
+
+
