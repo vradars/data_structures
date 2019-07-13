33f33c5018848383ef9c52c2b488ea56d85833c1
vanshika <vanshikachd@gmail.com>
2019-07-13 11:51:50 +0530

    bubble sort

---
+++ bubblesort.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+int n,temp;
+scanf("%d",&n);
+int arr[n];
+for(int i=0;i<n;i++)
+{
+scanf("%d",&arr[i]);
+}
+for(int i=0;i<n;i++)
+{
+for(int j=i+1;j<n;j++)
+{
+if(arr[i]>arr[j])
+{
+temp=arr[i];
+arr[i]=arr[j];
+arr[j]=temp;
+}
+}
+}
+for(int i=0;i<n;i++)
+{
+printf("%d",arr[i]);
+}
+}
